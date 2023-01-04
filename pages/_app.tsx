620d78dad1c82fe89151e592ec6aa4669f0871fb
willman orellano <wilmanjunior2001@gmail.com>
2023-01-04 03:23:40 -0500

    configuracion del proyecto (ts, tailwindcss, nextUI, scss)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import  {  NextUIProvider  }  from  "@nextui-org/react" 
+import  {  ThemeProvider  as  NextThemesProvider  }  from  'next-themes' ;
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return(
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  ) 
 }
